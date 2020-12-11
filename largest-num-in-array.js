dc0c7f2b62c5fa196761c15fd66a8f2eb6bddf4e
Josh Allan <63212805+jallan07@users.noreply.github.com>
2020-12-11 09:34:46 -0500

    Update largest-num-in-array.js

--- largest-num-in-array.js
+++ largest-num-in-array.js
@@ -1,15 +1,11 @@
 // returns the largest number in an arry
-
 const maxNum = (arr) => {
   let max = arr[0];
-
   for (let i = 1; i < arr.length; i++) {
     let currentNum = arr[i];
-
     if (currentNum > max) {
       max = currentNum;
     }
   }
-
   return max;
 };
